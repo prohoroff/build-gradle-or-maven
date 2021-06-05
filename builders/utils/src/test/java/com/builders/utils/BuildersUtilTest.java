0f409f210cd585258accf5a092d2976dcbbbcd67
Artsiom Prokharau <artem.prohoroff@gmail.com>
2021-06-05 12:08:58 +0300

    Add Gradle and Maven

---
+++ builders/utils/src/test/java/com/builders/utils/BuildersUtilTest.java
@@ -0,0 +1,25 @@
+package com.builders.utils;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.junit.Assert.assertNotNull;
+
+public class BuildersUtilTest {
+
+    @Test
+    public void testHello() {
+        InputStream stream = BuildersUtil.class.getClassLoader().getResourceAsStream("utils_config_test.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertNotNull(properties.getProperty("test_hello"));
+    }
+
+}
